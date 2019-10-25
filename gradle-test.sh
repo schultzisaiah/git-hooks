221ff2e718afeb20901ccbef19b94cd677cdbb1d
Isaiah Schultz <isaiah.schultz@tr.com>
2019-10-25 15:35:04 -0500

    adding gradle test script

---
+++ gradle-test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# This script should be copied to the "hooks" subdirectory of your project's
+# ".git" directory. If you want it to run before every commit, then rename this
+# file to "pre-commit" (with NO file extension). If you want to run it before
+# every push instead, then rename it to "pre-push" (with NO file extension).
+# Make sure that this file is executable.
+#
+# If done properly, then git will automatically run this script before every
+# commit/push!
+
+gradle test 2>&1 | tee commit.log
+grep -q "BUILD SUCCESS" commit.log
+exit $?
